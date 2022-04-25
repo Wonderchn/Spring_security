9128e947108e081d30af28791e5322ad83f7af61
Wonderchn <a1073506500@QQ.COM>
2022-04-25 18:02:32 +0800

    2022年4月25日 用户校验前准备

---
+++ TokenDemo/src/main/java/hongna/TokenApplication.java
@@ -0,0 +1,11 @@
+package hongna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TokenApplication.class, args);
+    }
+}

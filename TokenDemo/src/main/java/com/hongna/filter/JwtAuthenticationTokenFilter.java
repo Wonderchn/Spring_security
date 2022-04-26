07967f3abd2bf389e033e92463c10c2bde48cdeb
Wonderchn <a1073506500@QQ.COM>
2022-04-26 17:49:05 +0800

    2022年4月26日 代码提交

---
+++ TokenDemo/src/main/java/com/hongna/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,21 @@
+package com.hongna.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //获取token
+        String token = request.getHeader("token");
+
+        //从redis中获取
+    }
+}
